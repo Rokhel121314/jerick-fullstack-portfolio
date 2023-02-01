8d49bd71bfc07dc30d12f47f5425663dc3ec2283
Rokhel121314 <jerickddelacruz@gmail.com>
2023-02-01 13:44:13 +0800

    include link for project

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -9,27 +9,31 @@ function ProjectsPage() {
       <section className="projectsSection">
         {/* PROJECT CARD 1*/}
         <div className="projectsCard">
-          <span className="projectTitle">LAKO PH</span>
+          <a href="https://lakoph.online/" className="projectTitle">
+            <span>LAKO PH</span>
+          </a>
 
           <img
             src={require("../assets/projects/lakoGIF1.gif")}
             alt="projectimages"
             className="projectImage1"
           />
-
-          <div className="projectTitle">
-            <div className="bottomBorder">
-              <span className="projectsDetail">
-                PROVIDES STOCKS, POS, TRANSACTION AND SALES SUMMARY FOR SMALL
-                BUSINESS SUCH AS STORES, BAKERIES AND MANY MORE
-              </span>
-            </div>
+          <div className="bottomBorder">
+            <span className="projectsDetail">
+              PROVIDES STOCKS, POS, TRANSACTION AND SALES SUMMARY FOR SMALL
+              BUSINESS SUCH AS STORES, BAKERIES AND MANY MORE
+            </span>
           </div>
         </div>
         {/* END OF PROJECT CARD */}
         {/* PROJECT CARD 2*/}
         <div className="projectsCard">
-          <span className="projectTitle">STICK WARS</span>
+          <a
+            href="https://rokhel121314.github.io/stickWars_v.1.0/"
+            className="projectTitle"
+          >
+            <span>STICK WARS</span>
+          </a>
 
           <img
             src={require("../assets/projects/stickwarsGIF.gif")}
@@ -37,19 +41,23 @@ function ProjectsPage() {
             className="projectImage1"
           />
 
-          <div className="projectTitle">
-            <div className="bottomBorder">
-              <span className="projectsDetail">
-                A SIMPLE FIGHTING GAME WITH PLAYER VS PLAYER AND PLAYER VS BOT,
-                MADE WITH JAVASCRIPT.
-              </span>
-            </div>
+          <div className="bottomBorder">
+            <span className="projectsDetail">
+              A SIMPLE FIGHTING GAME WITH PLAYER VS PLAYER AND PLAYER VS BOT,
+              MADE WITH JAVASCRIPT.
+            </span>
           </div>
         </div>
+
         {/* END OF PROJECT CARD */}
         {/* PROJECT CARD 3*/}
         <div className="projectsCard">
-          <span className="projectTitle">TEMPERATURE CONVERTER</span>
+          <a
+            href="https://rokhel121314.github.io/TemperatureConverterv.3.0/"
+            className="projectTitle"
+          >
+            <span>TEMPERATURE CONVERTER</span>
+          </a>
 
           <img
             src={require("../assets/projects/tempconvertGIF.gif")}
@@ -57,15 +65,14 @@ function ProjectsPage() {
             className="projectImage1"
           />
 
-          <div className="projectTitle">
-            <div className="bottomBorder">
-              <span className="projectsDetail">
-                SIMPLE COMPLETE TEMPERATURE CONVERTER CAN BE USED FOR
-                ENGINEERING SUBJECTS
-              </span>
-            </div>
+          <div className="bottomBorder">
+            <span className="projectsDetail">
+              SIMPLE COMPLETE TEMPERATURE CONVERTER CAN BE USED FOR ENGINEERING
+              SUBJECTS
+            </span>
           </div>
         </div>
+
         {/* END OF PROJECT CARD */}
         {/* PROJECT CARD 4*/}
         <div className="projectsCard">
