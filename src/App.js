cedd1d31b76fa7ccbcaf1899f131a5a288defcdf
Rokhel121314 <jerickddelacruz@gmail.com>
2023-02-03 14:38:37 +0800

    make admin page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import ProjectsPage from "./pages/ProjectsPage";
 import SkillsPage from "./pages/SkillsPage";
 import ContactsPage from "./pages/ContactsPage";
 import Navigation from "./components/Navigation";
+import { Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin";
 
 export const URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
   return (
     <>
+      <Navigation />
       <LandingPage />
       <AboutMePage />
       <ProjectsPage />
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Navigation from "./components/Navigation";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Admin from "./pages/Admin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <StrictMode>
-      <BrowserRouter>
-        <Navigation />
-        <App />
-      </BrowserRouter>
-    </StrictMode>
+    {/* <StrictMode> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+    {/* </StrictMode> */}
   </>
 );
